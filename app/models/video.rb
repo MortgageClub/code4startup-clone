d4fae45210ee41333ebe9555b9dc5cbadac76579
TangNV <nvtang.khtn@gmail.com>
2015-12-16 08:55:03 +0700

    remove required video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,7 @@
 class Video < ActiveRecord::Base
   validates :wistia, length: {maximum: 10}, uniqueness: true
   validates :title, length: {maximum: 40}, presence: true
-  validates :description, length: {maximum: 800}, presence: true
+  validates :description, length: {maximum: 800}
 
   def next
     Video.where("id > ?", id).first
